f5a3a3845c4ce27425cd16fda21871ba401a3304
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2021-05-20 03:27:08 -0400

    Preliminary layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Theo Simulator',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
       home: MyHomePage(title: 'Theo Simulator'),
@@ -27,15 +17,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -63,95 +44,71 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Column(
-        // Column is also a layout widget. It takes a list of children and
-        // arranges them vertically. By default, it sizes itself to fit its
-        // children horizontally, and tries to be as tall as its parent.
-        //
-        // Invoke "debug painting" (press "p" in the console, choose the
-        // "Toggle Debug Paint" action from the Flutter Inspector in Android
-        // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-        // to see the wireframe for each widget.
-        //
-        // Column has various properties to control how it sizes itself and
-        // how it positions its children. Here we use mainAxisAlignment to
-        // center the children vertically; the main axis here is the vertical
-        // axis because Columns are vertical (the cross axis would be
-        // horizontal).
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Image(
-            image: _image,
-            width: 300,
-            height: 300,
-          ),
-          Container(
-            width: 300,
-            height: 150,
-            color: Colors.green,
-            child: Row(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          color: Colors.yellow,
+          child: SingleChildScrollView(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      color: Colors.red,
-                      child: Text(
-                        "Make Poo:",
-                      ),
-                    )
-                  ],
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: _image,
+                  width: 300,
+                  height: 300,
                 ),
-                Column(
-                  // mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildButton("Happy", "TheoHappy.jpg"),
-                    _buildButton("Zone Out", "TheoZoneOut.jpg"),
-                    _buildButton("Old-Timey", "TheoOldTimey.jpg"),
-                    _buildButton("Amiable", "TheoBase.jpg"),
-                  ],
+                IntrinsicHeight(
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Make Theo:",
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            _buildButton("Happy", "TheoHappy.jpg"),
+                            _buildButton("Zone Out", "TheoZoneOut.jpg"),
+                            _buildButton("Old-Timey", "TheoOldTimey.jpg"),
+                            _buildButton("Amiable", "TheoBase.jpg"),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            _buildButton("Hungry", "TheoHungry.jpg"),
+                            _buildButton("Smirk", "TheoSmirk.jpg"),
+                            _buildButton("Playful", "TheoPlayful.jpg"),
+                            _buildButton("Cry", "TheoCry.jpg"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    _buildButton("Hungry", "TheoHungry.jpg"),
-                    _buildButton("Smirk", "TheoSmirk.jpg"),
-                    _buildButton("Playful", "TheoPlayful.jpg"),
-                    _buildButton("Cry", "TheoCry.jpg"),
-                  ],
-                ),
+                // Text(
+                //   '$_counter',
+                //   style: Theme.of(context).textTheme.headline4,
+                // ),
+                _buildButton("Bright-Eyed & Bushy-Tailed",
+                    "TheoBrightEyedBushyTailed.jpg"),
               ],
             ),
           ),
-
-          // Text(
-          //   '$_counter',
-          //   style: Theme.of(context).textTheme.headline4,
-          // ),
-          // _buildButton(
-          // "Bright-Eyed & Bushy-Tailed", "TheoBrightEyedBushyTailed.jpg"),
-        ],
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: () => setImage("TheoOldTimey"),
-      //   tooltip: 'Increment',
-      //   child: Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () => setImage("TheoOldTimey"),
+          //   tooltip: 'Increment',
+          //   child: Icon(Icons.add),
+          // ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 }
