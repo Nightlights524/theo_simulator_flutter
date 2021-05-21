683c41e2d55c5214734c63eb5af7039a17be37c9
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2021-05-20 23:49:28 -0400

    Temp Layout 3

--- lib/main.dart
+++ lib/main.dart
@@ -59,134 +59,136 @@ class _MyHomePageState extends State<MyHomePage> {
         // padding: EdgeInsets.all(20),
         // width: 340,
         color: Colors.purple.shade50,
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                _imagePath,
-                fit: BoxFit.cover,
-                width: 300,
-                height: 300,
-                gaplessPlayback: true,
-              ),
-              // Expanded(
-              // child: Container(
-              Container(
-                constraints: BoxConstraints(
-                    // maxWidth: 300,
-                    // minWidth: 300,
-                    // minHeight: 400,
-                    // maxHeight: 200,
-                    ),
-                width: 300,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    // Expanded(
-                    //   child: Column(
-                    //     mainAxisAlignment: MainAxisAlignment.start,
-                    //     children: [
-                    //       Text(
-                    //         "Make Theo:",
-                    //         style: TextStyle(
-                    //             fontSize: 21, fontWeight: FontWeight.bold),
-                    //       ),
-                    //     ],
-                    //   ),
-                    // ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Make Theo:",
-                          style: TextStyle(
-                              fontSize: 21, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  _imagePath,
+                  fit: BoxFit.cover,
+                  width: 300,
+                  height: 300,
+                  gaplessPlayback: true,
+                ),
+                // Expanded(
+                // child: Container(
+                Container(
+                  constraints: BoxConstraints(
+                      // maxWidth: 300,
+                      // minWidth: 300,
+                      // minHeight: 400,
+                      // maxHeight: 200,
+                      ),
+                  width: 300,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Expanded(
+                      //   child: Column(
+                      //     mainAxisAlignment: MainAxisAlignment.start,
+                      //     children: [
+                      //       Text(
+                      //         "Make Theo:",
+                      //         style: TextStyle(
+                      //             fontSize: 21, fontWeight: FontWeight.bold),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          Row(
-                            children: [
-                              _buildButton("Happy", "TheoHappy.jpg"),
-                              _buildButton("Hungry", "TheoHungry.jpg"),
-                              // Expanded(child: _buildButton("Happy", "TheoHappy.jpg")),
-                              // Expanded(child: _buildButton("Hungry", "TheoHungry.jpg")),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              _buildButton("Zone Out", "TheoZoneOut.jpg"),
-                              _buildButton("Smirk", "TheoSmirk.jpg"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              _buildButton("Old-Timey", "TheoOldTimey.jpg"),
-                              _buildButton("Playful", "TheoPlayful.jpg"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              _buildButton("Bright-Eyed & Bushy-Tailed",
-                                  "TheoBrightEyedBushyTailed.jpg"),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              _buildButton("Amiable", "TheoBase.jpg"),
-                              _buildButton("Cry", "TheoCry.jpg"),
-                            ],
+                          Text(
+                            "Make Theo:",
+                            style: TextStyle(
+                                fontSize: 21, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
-                    ),
-                    // Column(
-                    //   crossAxisAlignment: CrossAxisAlignment.start,
-                    //   children: [
-                    //     Row(
-                    //       children: [
-                    //         _buildButton("Happy", "TheoHappy.jpg"),
-                    //         _buildButton("Hungry", "TheoHungry.jpg"),
-                    //         // Expanded(child: _buildButton("Happy", "TheoHappy.jpg")),
-                    //         // Expanded(child: _buildButton("Hungry", "TheoHungry.jpg")),
-                    //       ],
-                    //     ),
-                    //     Row(
-                    //       children: [
-                    //         _buildButton("Zone Out", "TheoZoneOut.jpg"),
-                    //         _buildButton("Smirk", "TheoSmirk.jpg"),
-                    //       ],
-                    //     ),
-                    //     Row(
-                    //       children: [
-                    //         _buildButton("Old-Timey", "TheoOldTimey.jpg"),
-                    //         _buildButton("Playful", "TheoPlayful.jpg"),
-                    //       ],
-                    //     ),
-                    //     Row(
-                    //       children: [
-                    //         _buildButton("Bright-Eyed & Bushy-Tailed",
-                    //             "TheoBrightEyedBushyTailed.jpg"),
-                    //       ],
-                    //     ),
-                    //     Row(
-                    //       children: [
-                    //         _buildButton("Amiable", "TheoBase.jpg"),
-                    //         _buildButton("Cry", "TheoCry.jpg"),
-                    //       ],
-                    //     ),
-                    //   ],
-                    // ),
-                  ],
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                _buildButton("Happy", "TheoHappy.jpg"),
+                                _buildButton("Hungry", "TheoHungry.jpg"),
+                                // Expanded(child: _buildButton("Happy", "TheoHappy.jpg")),
+                                // Expanded(child: _buildButton("Hungry", "TheoHungry.jpg")),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildButton("Zone Out", "TheoZoneOut.jpg"),
+                                _buildButton("Smirk", "TheoSmirk.jpg"),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildButton("Old-Timey", "TheoOldTimey.jpg"),
+                                _buildButton("Playful", "TheoPlayful.jpg"),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildButton("Bright-Eyed & Bushy-Tailed",
+                                    "TheoBrightEyedBushyTailed.jpg"),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                _buildButton("Amiable", "TheoBase.jpg"),
+                                _buildButton("Cry", "TheoCry.jpg"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      // Column(
+                      //   crossAxisAlignment: CrossAxisAlignment.start,
+                      //   children: [
+                      //     Row(
+                      //       children: [
+                      //         _buildButton("Happy", "TheoHappy.jpg"),
+                      //         _buildButton("Hungry", "TheoHungry.jpg"),
+                      //         // Expanded(child: _buildButton("Happy", "TheoHappy.jpg")),
+                      //         // Expanded(child: _buildButton("Hungry", "TheoHungry.jpg")),
+                      //       ],
+                      //     ),
+                      //     Row(
+                      //       children: [
+                      //         _buildButton("Zone Out", "TheoZoneOut.jpg"),
+                      //         _buildButton("Smirk", "TheoSmirk.jpg"),
+                      //       ],
+                      //     ),
+                      //     Row(
+                      //       children: [
+                      //         _buildButton("Old-Timey", "TheoOldTimey.jpg"),
+                      //         _buildButton("Playful", "TheoPlayful.jpg"),
+                      //       ],
+                      //     ),
+                      //     Row(
+                      //       children: [
+                      //         _buildButton("Bright-Eyed & Bushy-Tailed",
+                      //             "TheoBrightEyedBushyTailed.jpg"),
+                      //       ],
+                      //     ),
+                      //     Row(
+                      //       children: [
+                      //         _buildButton("Amiable", "TheoBase.jpg"),
+                      //         _buildButton("Cry", "TheoCry.jpg"),
+                      //       ],
+                      //     ),
+                      //   ],
+                      // ),
+                    ],
+                  ),
                 ),
-              ),
-              // ),
-            ],
+                // ),
+              ],
+            ),
           ),
         ),
       ),
