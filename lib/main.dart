8d6a71007295586cb868ff223e6d3c4f67898612
Nightlights524 <41940695+Nightlights524@users.noreply.github.com>
2021-05-22 22:30:06 -0400

    Continue responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() => runApp(TheoSimulator());
 
@@ -6,15 +7,37 @@ class TheoSimulator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const String appTitle = "Theo Simulator";
-    return MaterialApp(
-      title: appTitle,
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+    return ScreenUtilInit(
+      designSize: Size(390, 844),
+      // designSize: Size(1170, 2532),
+      builder: () => MaterialApp(
+        title: appTitle,
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          // textTheme: TextTheme(
+          //   button: TextStyle(fontSize: 45.sp),
+          // ),
+        ),
+        home: MyHomePage(title: appTitle),
       ),
-      home: MyHomePage(title: appTitle),
     );
   }
 }
+
+// class TheoSimulator extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     const String appTitle = "Theo Simulator";
+//     return
+//     MaterialApp(
+//       title: appTitle,
+//       theme: ThemeData(
+//         primarySwatch: Colors.deepPurple,
+//       ),
+//       home: MyHomePage(title: appTitle),
+//     );
+//   }
+// }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -62,14 +85,17 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildButton(String buttonText, String imageName) {
     return Expanded(
       flex: 10,
-      child: ElevatedButton(
-        onPressed: () => _setImage(imageName),
-        style: ElevatedButton.styleFrom(
-            // tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            ),
-        child: Text(
-          buttonText,
-          style: TextStyle(fontSize: 18),
+      child: Container(
+        margin: EdgeInsets.only(top: 10),
+        child: ElevatedButton(
+          onPressed: () => _setImage(imageName),
+          style: ElevatedButton.styleFrom(
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          child: Text(
+            buttonText,
+            style: TextStyle(fontSize: 18),
+          ),
         ),
       ),
     );
@@ -77,7 +103,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var mediaQueryData = MediaQuery.of(context);
+    final mediaQueryData = MediaQuery.of(context);
+    final isPortrait = mediaQueryData.orientation == Orientation.portrait;
+
     return Scaffold(
       backgroundColor: Colors.purple.shade50,
       appBar: AppBar(
@@ -88,54 +116,71 @@ class _MyHomePageState extends State<MyHomePage> {
           SliverFillRemaining(
             hasScrollBody: false,
             child: Padding(
-              padding: const EdgeInsets.all(30.0),
+              padding: const EdgeInsets.all(20.0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Image.asset(
                     _currentImage,
                     fit: BoxFit.cover,
-                    width: mediaQueryData.size.width - 60,
-                    height: mediaQueryData.size.width - 60,
+                    // width: 300.w,
+                    // height: 300.w,
+                    width: isPortrait ? 300.w : 350,
+                    height: isPortrait ? 300.w : 350,
+                    // width: mediaQueryData.size.width - 60,
+                    // height: mediaQueryData.size.width - 60,
                     // gaplessPlayback: true,
                   ),
-                  Text(
-                    "Make Theo:",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  Row(
-                    children: [
-                      _buildButton("Happy", "TheoHappy.jpg"),
-                      Spacer(),
-                      _buildButton("Hungry", "TheoHungry.jpg"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      _buildButton("Zone Out", "TheoZoneOut.jpg"),
-                      Spacer(),
-                      _buildButton("Smirk", "TheoSmirk.jpg"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      _buildButton("Old-Timey", "TheoOldTimey.jpg"),
-                      Spacer(),
-                      _buildButton("Playful", "TheoPlayful.jpg"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      _buildButton("Bright-Eyed & Bushy-Tailed",
-                          "TheoBrightEyedBushyTailed.jpg"),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      _buildButton("Amiable", "TheoAmiable.jpg"),
-                      Spacer(),
-                      _buildButton("Cry", "TheoCry.jpg"),
-                    ],
+                  Container(
+                    // width: 320.w,
+                    width: isPortrait ? 300.w : 350,
+                    child: Column(
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 10),
+                          child: Text(
+                            "Make Theo:",
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            _buildButton("Happy", "TheoHappy.jpg"),
+                            Spacer(),
+                            _buildButton("Hungry", "TheoHungry.jpg"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildButton("Zone Out", "TheoZoneOut.jpg"),
+                            Spacer(),
+                            _buildButton("Smirk", "TheoSmirk.jpg"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildButton("Old-Timey", "TheoOldTimey.jpg"),
+                            Spacer(),
+                            _buildButton("Playful", "TheoPlayful.jpg"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildButton("Bright-Eyed & Bushy-Tailed",
+                                "TheoBrightEyedBushyTailed.jpg"),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            _buildButton("Amiable", "TheoAmiable.jpg"),
+                            Spacer(),
+                            _buildButton("Cry", "TheoCry.jpg"),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
